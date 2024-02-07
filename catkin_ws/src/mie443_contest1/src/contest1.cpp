7753fc72ff3297e8f830e201744aca1bc27828a5
Cherry <cherry.lian@mail.utoronto.com>
2024-02-06 20:05:15 -0500

    fixed collision limit

--- catkin_ws/src/mie443_contest1/src/contest1.cpp
+++ catkin_ws/src/mie443_contest1/src/contest1.cpp
@@ -20,7 +20,6 @@
 float angular = 0.0;
 float linear = 0.0;
 
-//float posX = 0.0, posY = 0.0, yaw = 0.0;
 double posX = 0.0, posY = 0.0, yaw = 0.0;
 
 uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
@@ -34,7 +33,6 @@ int32_t nLasers=0, desiredNLasers=0, desiredAngle=5;
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-	//fill with your code
     // LEFT, CENTER, or RIGHT
     bumper[msg->bumper] = msg->state;
 }
@@ -61,7 +59,6 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 void odomCallback (const nav_msgs::Odometry::ConstPtr& msg)
 {
-    //fill w code
     posX = msg->pose.pose.position.x;
     posY = msg->pose.pose.position.y;
     yaw = tf::getYaw(msg->pose.pose.orientation);
@@ -90,92 +87,117 @@ int main(int argc, char **argv)
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
 
-    // float angular = 0.0;
-    // float linear = 0.0;
 
     // initialize loop variables
-    int spin_countdown = 4;
     bool scan_360 = true;
     bool change_turn_direction = false;
+    float collision_turn = M_PI / 9;
+    float reg_turn = M_PI / 3;
+    float initial_yaw = yaw;
+    float final_yaw = initial_yaw - M_PI / 8 + 2 * M_PI;
+    float prev_yaw = yaw;
 
     while(ros::ok() && secondsElapsed <= 480) {
         ros::spinOnce();
+
+        if (yaw < 0) {
+            yaw += 2 * M_PI;
+        }
+
+        float yaw_diff = abs(yaw - prev_yaw);
         
         // check if bumper pressed
         bool any_bumper_pressed = false;
+        int bumper_num = -1;
         for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx) {
             any_bumper_pressed |=(bumper[b_idx]==kobuki_msgs::BumperEvent::PRESSED);
+            if (bumper[b_idx]==kobuki_msgs::BumperEvent::PRESSED) {
+                bumper_num = b_idx;
+            }
         }
 
-        //Control logic after bumpers are being pressed
-        // ROS_INFO("Position: (%f, %f) Orientation: %f degrees Range: %f", posX, posY, RAD2DEG(yaw), minLaserDist);
-
+        // Control logic after bumpers are being pressed
+        ROS_INFO("Position: (%f, %f) Orientation: %f degrees Range: %f", posX, posY, RAD2DEG(yaw), minLaserDist);
 
         // spin 360 to scan area
         if (scan_360) {
             ROS_INFO("scanning 360 deg");
-            angular = M_PI / 2;
+            angular = reg_turn;
             linear = 0.0;
-            spin_countdown -= 1;
 
-            // once countdown hits zero, stop spinning
-            if (- M_PI/ 100 < yaw < - M_PI / 120) {
+            // stop spinning after 360
+            if (yaw > final_yaw) {
                 ROS_INFO("done scanning 360 deg");
                 scan_360 = false;
-                // spin_countdown = 4;
                 angular = 0.0;
                 linear = 0.0;
             }
         }
 
-        // // not spinning anymore, proceed to regular mapping conditions
-        // else {
+        // not spinning anymore, proceed to regular mapping conditions
+        else {
 
-        //     // if bumper pressed, move backwards and turn 20 deg
-        //     if (any_bumper_pressed) {
-        //         ROS_INFO("bumper pressed, too close! (attempt to back up and turn slightly)");
-        //         angular = M_PI / 9;
-        //         // linear = 0.01;
-        //         linear = -0.01;
-        //     }
+            // if bumper pressed, move backwards and turn 20 deg
+            if (any_bumper_pressed) {
+                ROS_INFO("bumper pressed, too close! (attempt to back up and turn slightly)");
 
-        //     // if min laser distance less than 0.7m, turn 
-        //     else if (minLaserDist < 0.7) {
-        //         ROS_INFO("laser distance < 0.7m, need to turn! (attempt to turn left/right to avoid hitting obstacles)");
-        //         linear = 0.0;
-        //         angular = M_PI / 2;
-                
-        //     }
+                switch (bumper_num) {
 
-        //     // if yaw is less than 
-        //     else if (yaw < M_PI / 120 && yaw < M_PI / 120) {
-        //         ROS_INFO("cruising");
-        //         linear = 0.25;
-        //         angular = 0.0;
-        //     }
+                    case 0:
+                        angular = -collision_turn;
+                        break;
 
-        // }
+                    case 2:
+                        angular = collision_turn;
+                        break;
+                }
 
-        // if (secondsElapsed % 30 == 0) {
-        //     ROS_INFO("it's been 30 seconds, time to scan");
-        //     scan_360 == true;
-        // }
+                // angular = collision_turn;
+                // linear = 0.01;
+                linear = -0.01;
+            }
 
-        // if (change_turn_direction == true) {
-        //     ROS_INFO("turning right");
-        //     angular *= -1;
-        // }
+            // if min laser distance less than 0.7m, turn 
+            else if (minLaserDist < 1) {
+                ROS_INFO("laser distance < 1m, need to turn! (attempt to turn left/right to avoid hitting obstacles)");
+                linear = 0.0;
+                angular = reg_turn;
+            }
 
-        // else if (posX < 0.5 && posY < 0.5 && secondsElapsed > 120) {
-        //     ROS_INFO("changed to turning right");
-        //     change_turn_direction == true;
-        // } 
+            // no obstacles, go straight
+            else if (yaw_diff > reg_turn) {
+                ROS_INFO("cruising");
+                linear = 0.25;
+                angular = 0.0;
+            }
+
+            if (secondsElapsed % 30 == 0) {
+                ROS_INFO("it's been 30 seconds, time to scan");
+                scan_360 = true;
+                initial_yaw = yaw;
+                final_yaw = initial_yaw - M_PI / 8;
+                if (final_yaw < 0) {
+                    final_yaw += 2 * M_PI;
+
+                // ROS_INFO("final yaw: ", RAD2DEG(final_yaw));
+                }
+            }
+
+            if (change_turn_direction) {
+                ROS_INFO("turning right");
+                angular *= -1;
+            }
+
+            else if (posX < 0.5 && posY < 0.5 && secondsElapsed > 120) {
+                ROS_INFO("changed to turning right");
+                change_turn_direction = true;
+            } 
+
+        }
 
         vel.angular.z = angular;
         vel.linear.x = linear;
         vel_pub.publish(vel);
-
-        
 
         // The last thing to do is to update the timer.
         secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
