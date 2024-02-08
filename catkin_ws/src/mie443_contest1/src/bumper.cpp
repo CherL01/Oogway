434803aa307e7138aec1d63d2d68954e439c2977
Harry Park <hyungsup.park@mail.utoronto.ca>
2024-02-07 23:15:47 -0500

    initializing Harry

---
+++ catkin_ws/src/mie443_contest1/src/bumper.cpp
@@ -0,0 +1,17 @@
+#include "bumper.h"
+
+uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+
+uint8_t leftState = bumper[kobuki_msgs::BumperEvent::LEFT];
+uint8_t rightState = bumper[kobuki_msgs::BumperEvent::RIGHT];
+uint8_t centerState = bumper[kobuki_msgs::BumperEvent::CENTER];
+
+void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+{
+    bumper[msg->bumper] = msg->state;
+    leftState = bumper[0];
+    centerState = bumper[1];
+    rightState = bumper[2];
+
+    ROS_INFO("Left Bumper: %i, Center Bumper: %i, Right Bumper: %i", leftState, centerState, rightState);
+}
---
+++ catkin_ws/src/mie443_contest1/src/globals.h
@@ -0,0 +1,54 @@
+#ifndef GLOBAL_HEADER
+#define GLOBAL_HEADER
+
+//libraries needed 
+
+#include <ros/console.h>
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+#include <kobuki_msgs/BumperEvent.h>
+#include <sensor_msgs/LaserScan.h>
+
+#include <stdio.h>
+#include <cmath>
+
+#include <chrono>
+
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+
+// equations, definitions
+#define N_BUMPER (3)
+#define RAD2DEG(rad) ((rad)*180. /M_PI)
+#define DEG2RAD(deg) ((deg)* M_PI /180.)
+
+
+//global variables
+
+//velocities being published
+extern float angular;
+extern float linear;
+
+const float slowDownLimit = 0.8;
+const float stopLimit = 0.5;
+const float maxOpening = 0.95;
+const float midLimit = 0.7;
+
+//odometery variables
+extern float posX, posY, yaw;
+
+//bumper variables
+extern uint8_t bumper[3];
+extern bool anyBumperPressed;
+
+extern uint8_t leftState, rightState, centerState;
+
+
+
+
+//laser variables
+extern float minLaserDist, leftLaserDist, rightLaserDist;
+extern int32_t desiredAngle;
+extern int32_t nLasers, desiredNLasers;
+
+#endif
