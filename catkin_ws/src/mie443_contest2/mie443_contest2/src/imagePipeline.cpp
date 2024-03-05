cd7d86984f5d60feeecfc4103111126d73a3c0a5
henryyqi <henryqiz19@gmail.com>
2024-03-05 17:33:49 -0500

    mar 3

--- catkin_ws/src/mie443_contest2/mie443_contest2/src/imagePipeline.cpp
+++ catkin_ws/src/mie443_contest2/mie443_contest2/src/imagePipeline.cpp
@@ -1,5 +1,17 @@
 #include <imagePipeline.h>
 
+// Henry added
+using namespace cv;
+using namespace cv::xfeatures2d;
+using std::cout;
+using std::endl;
+
+//input1: box -> template, input2: boxinscene -> img
+const char* keys =
+        "{ help h |                          | Print help message. }"
+        "{ input1 | ../data/box.png          | Path to input image 1. }"
+        "{ input2 | ../data/box_in_scene.png | Path to input image 2. }";
+
 #define IMAGE_TYPE sensor_msgs::image_encodings::BGR8
 #define IMAGE_TOPIC "camera/rgb/image_raw" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
 
@@ -34,9 +46,61 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
         std::cout << "img.cols:" << img.cols << std::endl;
     } else {
         /***YOUR CODE HERE***/
+
+        Mat img_object = boxes.template[0];
+        Mat img_scene = img;
+        if ( img_object.empty() || img_scene.empty() )
+        {
+            cout << "Could not open or find the image!\n" << endl;
+            //parser.printMessage();
+            return -1;
+        }
+
+
+        //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+        int minHessian = 400;
+        Ptr<SURF> detector = SURF::create( minHessian );
+        std::vector<KeyPoint> keypoints_object, keypoints_scene;
+        Mat descriptors_object, descriptors_scene;
+        detector->detectAndCompute( img_object, noArray(), keypoints_object, descriptors_object );
+        detector->detectAndCompute( img_scene, noArray(), keypoints_scene, descriptors_scene );
+        
+        //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+        // Since SURF is a floating-point descriptor NORM_L2 is used
+        Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+        std::vector< std::vector<DMatch> > knn_matches;
+        matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
+        
+        //-- Filter matches using the Lowe's ratio test
+        const float ratio_thresh = 0.75f;
+        std::vector<DMatch> good_matches;
+        for (size_t i = 0; i < knn_matches.size(); i++)
+        {
+            if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+            {
+                good_matches.push_back(knn_matches[i][0]);
+            }
+        }
+
         // Use: boxes.templates
         cv::imshow("view", img);
         cv::waitKey(10);
     }  
     return template_id;
 }
+
+
+/*
+1. Get image input
+2. perform step 1: keypoint detection with SURF
+3. perform step 2: match descriptor vectors using feature matching
+4. use lowe's ratio to keep good matches
+5. good matches are visualized with drawMatches
+6. iterate good matches -> keypoints saved for obj and scene vectors respectively
+7. obj/scene transform using findHomography
+8. draw lines around it
+
+img -> the img we got from callback
+1. compare img with templates
+
+*/
