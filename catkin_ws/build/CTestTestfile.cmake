80d383c1787666bc726e12bfbf34572997491288
alastairsim <alastairsim6@gmail.com>
2024-03-18 10:04:47 -0400

    rm build devel

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tuesday2023/Oogway/catkin_ws/src
-# Build directory: /home/tuesday2023/Oogway/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(mie443_contest1)
-subdirs(mie443_contest2)
